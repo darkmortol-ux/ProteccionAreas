7162fc9d4c251be59d26b0ca2a93bd052fcecd4a
darkmortol-ux <darkmortol@gmail.com>
2026-08-05 00:08:52 -0600

    DeathListener.java

--- src/main/java/com/miplugin/protecciones/listeners/DeathListener.java
+++ src/main/java/com/miplugin/protecciones/listeners/DeathListener.java
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
